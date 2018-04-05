1a1c434b73c5da2d13e32956e3544e5b92debe26
wil <interianoe55@gmail.com>
2018-04-05 19:27:33 -0400

    Removed demo files

--- ios/rn-swift-bridge.m
+++ ios/rn-swift-bridge.m
@@ -1,14 +1,6 @@
-#import <React/RCTViewManager.h>
 #import <React/RCTEventEmitter.h>
 #import <React/RCTBridgeModule.h>
 @interface RCT_EXTERN_MODULE(mpg_lib, RCTEventEmitter)
 RCT_EXTERN_METHOD(showNotification:(NSString *)key title:(NSString *)title subtitle:(NSString *)subtitle hexBackgroundColor:(NSString *)hexBackgroundColor success:(RCTPromiseResolveBlock)success reject:(RCTPromiseRejectBlock)reject);
 RCT_EXTERN_METHOD(delayedSend:(NSString *)message ms:(NSInteger)ms);
-@end
-@interface RCT_EXTERN_MODULE(mpg_libBasicViewManager, RCTViewManager)
-@end
-@interface RCT_EXTERN_MODULE(mpg_libViewManager, RCTViewManager)
-RCT_EXTERN_METHOD(takePicture:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject);
-RCT_EXPORT_VIEW_PROPERTY(onStart, RCTBubblingEventBlock);
-RCT_EXPORT_VIEW_PROPERTY(cameraFront, BOOL);
 @end
