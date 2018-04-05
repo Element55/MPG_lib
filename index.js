1bea04d4c4b2b249e29c949075707983cf906e10
wil <interianoe55@gmail.com>
2018-04-05 15:45:32 -0400

    Removed unused nativemodule component

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-import NativeMod from "./nativemodule";
+import { NativeModules, NativeEventEmitter } from "react-native";
+const mpg_lib_native = NativeModules.mpg_lib;
 
-export { NativeMod };
+const showErrorNotification = async (title, subtitle) => {
+  showNotification(title, subtitle, "#FF0000");
+};
+const showSuccessNotification = async (title, subtitle) => {
+  showNotification(title, subtitle, "#00FF00");
+};
+const showNotification = async (title, subtitle = "", color = "#0000FF") => {
+  const output = await mpg_lib_native.showNotification(title, subtitle, color);
+  return output;
+};
+export { showErrorNotification, showSuccessNotification, showNotification };
