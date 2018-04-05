8b21b4daa7324d07369deb734d04b0b30bd9a561
wil <interianoe55@gmail.com>
2018-04-05 18:13:11 -0400

    Added support for showing notifications only after existing notification finishes firing

--- index.js
+++ index.js
@@ -1,14 +1,67 @@
 import { NativeModules, NativeEventEmitter } from "react-native";
 const mpg_lib_native = NativeModules.mpg_lib;
+const pendingNotifications = [];
+const isNotificationActive = false;
+const makeGuid = (
+  pieces,
+  delimiter,
+  prepend
+) => {
+  if (typeof pieces == "undefined") pieces = 4;
+  if (typeof delimiter == "undefined") delimiter = "-";
+  if (typeof prepend == "undefined") prepend = "";
+  const S4 = () => {
+    return Math.floor(Math.random() * 0x10000 /* 65536 */).toString(16);
+  };
 
-const showErrorNotification = async (title, subtitle) => {
-  showNotification(title, subtitle, "#FF0000");
+  const hashes = [];
+  for (var x = 0; x < pieces; x++) {
+    hashes.push(S4() + S4());
+  }
+  return prepend + hashes.join(delimiter);
 };
-const showSuccessNotification = async (title, subtitle) => {
-  showNotification(title, subtitle, "#00FF00");
+
+const e = new NativeEventEmitter(mpg_lib_native);
+const s = e.addListener("on_notification_tap", (d) => {
+  const { key } = d;
+  const n = pendingNotifications.find((n) => {
+    if (n.key === key) return true;
+    return false;
+  });
+  console.log(n, JSON.stringify(pendingNotifications))
+  if (n && typeof n.onTap == "function") {
+    n.onTap();
+  }
+});
+
+const showNotification = async (title, subtitle = "", color = "#0000FF", onTap) => {
+  const key = makeGuid();
+  console.log('sjhoNotification: start>', key, title)
+  pendingNotifications.push({
+    key,
+    title,
+    subtitle,
+    color,
+    onTap
+  });
+  showPendingNotifications();
 };
-const showNotification = async (title, subtitle = "", color = "#0000FF") => {
-  const output = await mpg_lib_native.showNotification(title, subtitle, color);
+const showPendingNotifications = async () => {
+  if (!pendingNotifications.length) return;
+
+  const thisNotification = pendingNotifications[0];
+  if (!thisNotification) return;
+  if (isNotificationActive) return;
+  isNotificationActive = true;
+  const { key, title, subtitle, color } = thisNotification;
+  console.log('showPenidngNotifications: pendingNotifications.length before>', pendingNotifications.length)
+  const output = await mpg_lib_native.showNotification(key, title, subtitle, color);
+  isNotificationActive = false;
+  pendingNotifications.shift();
+  console.log('showPenidngNotifications: pendingLocations.length: after>', pendingNotifications.length)
+  if (pendingNotifications.length) {
+    showPendingNotifications();
+  }
   return output;
-};
-export { showErrorNotification, showSuccessNotification, showNotification };
+}
+export { showNotification };
