ed9a230377b56fa731d0e25aaa0d5077eaa63495
wil <interianoe55@gmail.com>
2018-04-06 13:18:20 -0400

    Removed optional and added duration argument

--- ios/rn-swift-bridge.m
+++ ios/rn-swift-bridge.m
@@ -1,6 +1,6 @@
 #import <React/RCTEventEmitter.h>
 #import <React/RCTBridgeModule.h>
 @interface RCT_EXTERN_MODULE(mpg_lib, RCTEventEmitter)
-RCT_EXTERN_METHOD(showNotification:(NSString *)key title:(NSString *)title subtitle:(NSString *)subtitle hexBackgroundColor:(NSString *)hexBackgroundColor success:(RCTPromiseResolveBlock)success reject:(RCTPromiseRejectBlock)reject);
+RCT_EXTERN_METHOD(showNotification:(NSString *)key title:(NSString *)title subtitle:(NSString *)subtitle hexBackgroundColor:(NSString *)hexBackgroundColor duration:(double)duration success:(RCTPromiseResolveBlock)success reject:(RCTPromiseRejectBlock)reject);
 RCT_EXTERN_METHOD(delayedSend:(NSString *)message ms:(NSInteger)ms);
 @end
