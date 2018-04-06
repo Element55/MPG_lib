ef6c2e7a071e49fec68a6d6927b6ec308601893f
wil <interianoe55@gmail.com>
2018-04-06 13:28:08 -0400

    Added default options for notification

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ import { NativeModules, NativeEventEmitter } from "react-native";
 const mpg_lib_native = NativeModules.mpg_lib;
 const pendingNotifications = [];
 const isNotificationActive = false;
-const makeGuid = (
-  pieces,
-  delimiter,
-  prepend
-) => {
+const makeGuid = (pieces, delimiter, prepend) => {
   if (typeof pieces == "undefined") pieces = 4;
   if (typeof delimiter == "undefined") delimiter = "-";
   if (typeof prepend == "undefined") prepend = "";
@@ -22,47 +18,63 @@ const makeGuid = (
 };
 
 const e = new NativeEventEmitter(mpg_lib_native);
-const s = e.addListener("on_notification_tap", (d) => {
+const s = e.addListener("on_notification_tap", d => {
   const { key } = d;
-  const n = pendingNotifications.find((n) => {
+  const n = pendingNotifications.find(n => {
     if (n.key === key) return true;
     return false;
   });
-  console.log(n, JSON.stringify(pendingNotifications))
   if (n && typeof n.onTap == "function") {
     n.onTap();
   }
 });
-
-const showNotification = async (title, subtitle = "", color = "#0000FF", onTap) => {
+const defaultOptions = Object.freeze({
+  duration: 3,
+  backgroundColor: "#00FF00",
+  onTap: null
+});
+const showNotification = async (
+  title,
+  subtitle = "",
+  options = defaultOptions
+) => {
   const key = makeGuid();
-  console.log('sjhoNotification: start>', key, title)
+  if (typeof options !== "object") {
+    console.warn(
+      "showNotification: third argument must be an object. Valid keys: ",
+      JSON.stringify(Object.keys(defaultOptions))
+    );
+    options = defaultOptions;
+  }
   pendingNotifications.push({
     key,
     title,
     subtitle,
-    color,
-    onTap
+    options
   });
   showPendingNotifications();
   return key;
 };
 const showPendingNotifications = async () => {
   if (!pendingNotifications.length) return;
-
   const thisNotification = pendingNotifications[0];
   if (!thisNotification) return;
   if (isNotificationActive) return;
   isNotificationActive = true;
-  const { key, title, subtitle, color } = thisNotification;
-  console.log('showPenidngNotifications: pendingNotifications.length before>', pendingNotifications.length)
-  const output = await mpg_lib_native.showNotification(key, title, subtitle, color);
+  const { key, title, subtitle, options } = thisNotification;
+  const { duration, backgroundColor } = options;
+  const output = await mpg_lib_native.showNotification(
+    key,
+    title,
+    subtitle,
+    backgroundColor,
+    duration
+  );
   isNotificationActive = false;
   pendingNotifications.shift();
-  console.log('showPenidngNotifications: pendingLocations.length: after>', pendingNotifications.length)
   if (pendingNotifications.length) {
     showPendingNotifications();
   }
   return output;
-}
+};
 export { showNotification };
